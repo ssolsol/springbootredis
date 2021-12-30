bab506f11e903191565d1df050513f36d86bd69f
solji <solji1113@nexon.co.kr>
2021-12-30 10:49:11 +0900

    Initial commit

---
+++ src/test/java/me/whiteship/springbootredis/SpringbootredisApplicationTests.java
@@ -0,0 +1,13 @@
+package me.whiteship.springbootredis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootredisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
